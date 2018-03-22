57abb899b51a88d2ad30bbedf867a023cc22bb88
Amar Chatman <amarjerome@gmail.com>
2018-03-22 18:30:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,7 +83,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |event, supplies|
       if event == :fourth_of_july || :memorial_day
-        puts supplies
+        puts event
       end
     end
   end
